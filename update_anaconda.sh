d3d48becbccf8297a1931f3f102d62c6c9fe9c52
Florian Wagner <mail@fwagner.info>
2017-06-16 14:08:33 +0200

    fix mpl conda issue, add py3.6 build

--- update_anaconda.sh
+++ update_anaconda.sh
@@ -3,7 +3,7 @@ export PATH=~/miniconda3:$PATH
 rm -f ~/miniconda3/conda-bld/*.tar.bz2
 
 anaconda login
-for py in 3.5 3.4 2.7; do
+for py in 3.6 3.5 3.4 2.7; do
     conda build --python $py pygimli && anaconda upload --force ~/miniconda3/conda-bld/linux-64/pygimli-1.1rc-py${py//./}_0.tar.bz2
     conda build --python $py pybert && anaconda upload --force ~/miniconda3/conda-bld/linux-64/pybert-2.1.2-py${py//./}_0.tar.bz2
 done

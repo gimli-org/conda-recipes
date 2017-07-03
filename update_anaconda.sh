186831753d257ae003f5b1da0fa36a8360fa37b9
Florian Wagner <wagner@tirex.geo.uni-bonn.de>
2017-07-03 16:24:05 +0200

    flexible numpy version

--- update_anaconda.sh
+++ update_anaconda.sh
@@ -4,10 +4,13 @@ rm -f ~/miniconda3/conda-bld/*.tar.bz2
 
 anaconda login
 for py in 3.6 3.5 2.7; do
-    for pkg in pybert; do
-        name=`conda build $pkg --output --python $py`
-        echo "Building $name"
-        sleep 5
-        conda build -c conda-forge $pkg --python $py && anaconda upload --force $name
+    for pkg in pygimli pybert; do
+        for numpy in 1.12 1.13; do
+            params="$pkg --python $py --numpy $numpy"
+            name=`conda build $params --output`
+            echo "Building $name"
+            sleep 5
+            conda build -c conda-forge $params && anaconda upload --force $name
+        done
     done
 done

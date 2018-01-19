ad62929dab3484750e021a46642435b677569822
Florian Wagner <mail@fwagner.info>
2018-01-19 11:43:46 +0100

    Strip v from git tag for conda versions

--- update_anaconda.sh
+++ update_anaconda.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 export PATH=~/miniconda3:$PATH
-rm -f ~/miniconda3/conda-bld/*.tar.bz2
-rm -rf ~/miniconda3/conda-bld/pygimli_*
-rm -rf ~/miniconda3/conda-bld/pybert_*
+conda build purge # remove intermediate builds
 
 anaconda login
 for py in 3.6 3.5 2.7; do
-    for numpy in 1.12 1.13; do
+    for numpy in 1.13 1.14; do
         for pkg in pygimli pybert; do
             params="$pkg --python $py --numpy $numpy"
             name=`conda build $params --output`

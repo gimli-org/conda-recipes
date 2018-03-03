934f7c6a561612acc0b461a155ff40ecfb4a7183
Florian Wagner <mail@fwagner.info>
2018-03-03 17:21:41 +0100

    Skip existing builds (locally or in channel)

--- update_anaconda.sh
+++ update_anaconda.sh
@@ -3,14 +3,14 @@ export PATH=~/miniconda3:$PATH
 conda build purge # remove intermediate builds
 
 anaconda login
-for py in 3.6 3.5 2.7; do
-    for numpy in 1.13 1.14; do
+for py in 3.6 3.5; do
+    for numpy in 1.14 1.13; do
         for pkg in pygimli pybert; do
             params="$pkg --python $py --numpy $numpy"
             name=`conda build $params --output`
             echo "Building $name"
             sleep 5
-            conda build -c conda-forge $params && anaconda upload --force $name
+            conda build --skip-existing -c conda-forge $params && anaconda upload --force $name
         done
     done
 done

9a1bd08c48a9e40d881c6b0283c101fa1dcc65ed
Florian Wagner <mail@fwagner.info>
2019-04-02 19:07:07 +0200

    version bump

--- update_anaconda.sh
+++ update_anaconda.sh
@@ -2,11 +2,12 @@
 conda build purge # remove intermediate builds
 
 anaconda login
-for pkg in pygimli pybert; do
-    for py in 3.6 3.7; do
+#for pkg in pygimli pybert; do
+for pkg in pygimli; do
+    for py in 3.7 3.6; do
     name=`conda build $pkg --python $py --output`
     echo "Building $name"
     sleep 5
-    conda build -c defaults -c conda-forge --python $py $pkg && anaconda upload --force $name
+    conda build -c conda-forge --python $py $pkg && anaconda upload --force $name
 done
 done

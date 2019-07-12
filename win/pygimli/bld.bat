0d8bf3a7bbf19b92b04dcf7695bb8d5e370dcea2
Thomas Günther <thomas.guenther@liag-hannover.de>
2019-07-12 11:05:54 +0200

    add folder for simplified testing conda on Windows

---
+++ win/pygimli/bld.bat
@@ -0,0 +1,4 @@
+copy C:\Guenther.T\src\gimli\gimli37\python\pygimli\core\*.dll %LIBRARY_BIN%
+copy C:\Guenther.T\src\gimli\gimli37\python\pygimli\core\*.pyd %LIBRARY_BIN%
+rem %PYTHON% setup.py install --single-version-externally-managed record=record.txt
+python setup.py install --user

6c2441fb358481dd45d79f620ce81ae99f385372
Thomas Günther <thomas.guenther@liag-hannover.de>
2019-07-12 11:04:55 +0200

    remove erroneous #TODO in bld.bat by meaningful stuff (that is however not working)

--- pygimli/bld.bat
+++ pygimli/bld.bat
@@ -1,1 +1,3 @@
-#TODO
+%PYTHON% setup.py install
+copy C:\Guenther.T\src\gimli\gimli37\python\pygimli\core\*.dll %CONDA_PREFIX%\lib
+copy C:\Guenther.T\src\gimli\gimli37\python\pygimli\core\*.pyd %CONDA_PREFIX%\lib

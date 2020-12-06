01488404d46a4fba4294c0b4f1450cfa7ce27a2e
Florian Wagner <mail@fwagner.info>
2020-12-06 14:14:10 +0100

    bert 2.3.1

--- update_anaconda.sh
+++ update_anaconda.sh
@@ -2,8 +2,8 @@
 conda build purge # remove intermediate builds
 
 anaconda login
-#for pkg in pybert; do
-for pkg in pygimli; do
+for pkg in pybert; do
+#for pkg in pygimli; do
     for py in 3.7 3.8; do
     name=`conda build $pkg --python $py --output`
     echo "Building $name"

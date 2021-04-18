b7f156acb1eb252bf69a1f14e88d82ea37a40722
Florian Wagner <mail@fwagner.info>
2021-04-18 13:00:42 +0200

    1.2.1

--- update_anaconda.sh
+++ update_anaconda.sh
@@ -2,9 +2,9 @@
 conda build purge # remove intermediate builds
 
 anaconda login
-for pkg in pygimli; do
+for pkg in pgcore; do
 #for pkg in pygimli; do
-    for py in 3.7 3.8; do
+    for py in 3.8 3.7 3.9; do
     name=`conda build $pkg --python $py --output`
     echo "Building $name"
     sleep 5

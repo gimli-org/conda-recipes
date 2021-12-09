fdf585dfd988bfaff8426c4d90eed1f09e3897c8
Florian Wagner <mail@fwagner.info>
2021-12-09 12:20:20 +0100

    changes for 1.2.3

--- update_anaconda.sh
+++ update_anaconda.sh
@@ -2,12 +2,14 @@
 conda build purge # remove intermediate builds
 
 anaconda login
+conda config --set anaconda_upload yes
+
 for pkg in pgcore; do
-#for pkg in pygimli; do
-    for py in 3.8 3.7 3.9; do
+    #for py in 3.8 3.9 3.7 3.10; do
+    for py in 3.7; do # 3.9 3.7 3.10; do
     name=`conda build $pkg --python $py --output`
     echo "Building $name"
     sleep 5
-    conda build -c conda-forge --python $py $pkg && anaconda upload --force $name
+    conda build -c conda-forge --python $py $pkg
 done
 done

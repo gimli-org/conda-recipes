8213903b277b0b234d5d33b810cb6bce8d7154be
Florian Wagner <mail@fwagner.info>
2021-12-10 12:10:14 +0100

    Update update_anaconda.sh

--- update_anaconda.sh
+++ update_anaconda.sh
@@ -5,7 +5,7 @@ anaconda login
 conda config --set anaconda_upload yes
 
 for pkg in pgcore; do
-    for py in 3.7; do # 3.9 3.7 3.10; do
+    for py in 3.9 3.8 3.7 3.10; do
     #name=`conda build $pkg --python $py --output`
     #echo "Building $name"
     #sleep 5

4d77f10ebb383fba3a0347d06036fad67e299e20
carsten-forty2 <carsten@pygimli.org>
2022-08-19 14:11:40 +0200

    version bumps

--- update_anaconda.sh
+++ update_anaconda.sh
@@ -4,11 +4,13 @@ conda build purge # remove intermediate builds
 anaconda login
 conda config --set anaconda_upload yes
 
+##Build e.g., conda build -c conda-forge --python 3.8 pgcore
+
 for pkg in pgcore; do
     for py in 3.10 3.9 3.8; do
-    name=`conda build $pkg --python $py --output`
-    echo "Building $name"
-    sleep 3
-    conda build -c conda-forge --python $py $pkg
+        name=`conda build $pkg --python $py --output`
+        echo "Building $name"
+        sleep 3
+        conda build -c conda-forge --python $py $pkg
     done
 done
